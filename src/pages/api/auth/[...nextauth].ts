0f0a5cbd4032fae02e99e818345ca221ce5ca71f
GriffinSauce <jorisjgriffioen@gmail.com>
2021-01-19 23:39:58 +0100

    Sign JWT

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -60,9 +60,10 @@ const options = {
   // https://next-auth.js.org/configuration/options#jwt
   jwt: {
     // A secret to use for key generation (you should set this explicitly)
-    // secret: 'INp8IvdIyeMcoGAgFGoA61DdBglwwSqnXJZkgz8PSnw',
-    // Set to true to use encryption (default: false)
+    secret: process.env.JWT_SECRET,
+    signingKey: process.env.JWT_SIGNING_KEY,
     // encryption: true,
+    // encryptionKey: process.env.JWT_ENCRYPTION_KEY,
     // You can define your own encode/decode functions for signing and encryption
     // if you want to override the default behaviour.
     // encode: async ({ secret, token, maxAge }) => {},
