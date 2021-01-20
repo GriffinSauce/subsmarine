ff86cd29bc0395bd5df84f9b6cfc1ab306932917
GriffinSauce <jorisjgriffioen@gmail.com>
2021-01-20 23:28:52 +0100

    Better name

--- src/pages/stack/index.tsx
+++ src/pages/stack/index.tsx
@@ -1,14 +1,14 @@
 import { NextPage } from 'next';
 import Layout from 'components/Layout';
 import Container from 'components/Container';
-import Messages from 'components/MessageList';
+import MessageList from 'components/MessageList';
 
 const Page: NextPage = () => {
   return (
     <Layout>
       <Container>
         <h1>Stack</h1>
-        <Messages />
+        <MessageList />
       </Container>
     </Layout>
   );
