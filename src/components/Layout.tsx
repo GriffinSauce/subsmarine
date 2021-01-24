7c7c69608400fff115859fe4ec4a34e9a03fc981
GriffinSauce <jorisjgriffioen@gmail.com>
2021-01-24 18:21:29 +0100

    Create a single page with reading pane layout for desktop and mobile

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,20 @@
 import Header from 'components/Header';
 import Footer from 'components/Footer';
+import mergeClasses from 'utils/mergeClasses';
 
-const Layout: React.FC = ({ children }) => {
+interface Props {
+  fullHeight?: boolean;
+}
+
+const Layout: React.FC<Props> = ({ fullHeight = false, children }) => {
   return (
-    <>
+    <div
+      className={mergeClasses('flex flex-col', fullHeight && 'max-h-screen')}
+    >
       <Header />
-      <main>{children}</main>
+      <main className="flex flex-col min-h-0">{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
--- src/components/MessageListItem.tsx
+++ src/components/MessageListItem.tsx
@@ -1,8 +1,10 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { gmail_v1 } from 'googleapis';
 import { formatDistance } from 'date-fns';
-import { FiClock, FiMail } from 'react-icons/fi';
-import Link from 'next/link';
-import { getHeaderValue } from 'utils/message';
+import { FiClock, FiMail, FiCheck, FiSun } from 'react-icons/fi';
+import { getHeaderValue, getIsRead } from 'utils/message';
+import mergeClasses from 'utils/mergeClasses';
 
 interface Props {
   message: gmail_v1.Schema$Message;
@@ -24,6 +26,12 @@ const parseFrom = (fromString: string): From => {
 };
 
 const MessageListItem: React.FC<Props> = ({ message }) => {
+  const router = useRouter();
+  const params = (router.query.id as Array<string>) || []; // Catch-all is array
+  const [id] = params;
+
+  const isOpen = message.id === id;
+
   const fromString = getHeaderValue(message, 'From');
   const from = parseFrom(fromString);
 
@@ -33,11 +41,30 @@ const MessageListItem: React.FC<Props> = ({ message }) => {
   });
   const whenShort = when.replace(/^about /, '');
 
+  const isRead = getIsRead(message);
+
   return (
     <Link href={`/stack/${message.id}`}>
-      <a className="grid gap-1 leading-none">
-        <div className="inline-block font-bold text-blue-500">{from.name}</div>
-        <h2 className="text-base font-semibold truncate">
+      <a className="grid gap-1 leading-none text-left rounded focus:outline-none focus:ring ring-offset-4 ring-blue-200">
+        <div className="flex flex-row items-center space-x-2">
+          <span
+            className={mergeClasses(
+              'inline-block font-bold text-blue-500 rounded',
+              isRead && 'text-gray-500',
+              isOpen &&
+                'px-1 ring ring-blue-500 font-semibold bg-blue-500 text-white',
+            )}
+          >
+            {from.name}
+          </span>
+          {isRead ? <FiCheck /> : <FiSun />}
+        </div>
+        <h2
+          className={mergeClasses(
+            'leading-normal	font-semibold truncate',
+            isOpen && 'text-blue-700',
+          )}
+        >
           <span>{getHeaderValue(message, 'Subject')}</span>
           <span className="font-normal text-gray-400">
             {' '}
