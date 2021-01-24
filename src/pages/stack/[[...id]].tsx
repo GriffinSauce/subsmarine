de4ba4f6b404a097deb9de47e256eb8216bc9a40
GriffinSauce <jorisjgriffioen@gmail.com>
2021-01-24 18:33:30 +0100

    Document known issue

--- src/pages/stack/[[...id]].tsx
+++ src/pages/stack/[[...id]].tsx
@@ -8,6 +8,9 @@ import MessageList from 'components/MessageList';
 import Message from 'components/Message';
 
 const Page: NextPage = () => {
+  // Next.js does one render with empty params
+  // this triggers a flash of the list on mobile even when a message is selected
+  // TODO: use a hack? https://github.com/vercel/next.js/discussions/11484
   const router = useRouter();
   const params = (router.query.id as Array<string>) || []; // Catch-all is array
   const [id] = params;
