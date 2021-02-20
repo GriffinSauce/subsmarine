37a0095acf38046ad0ad4fa4314b19ab164ad87e
GriffinSauce <jorisjgriffioen@gmail.com>
2021-02-20 21:03:11 +0100

    First attempt at dark mode messages

--- src/components/MessageBody.tsx
+++ src/components/MessageBody.tsx
@@ -1,16 +1,22 @@
 import { gmail_v1 } from 'googleapis';
 import { getBodyHTML } from 'utils/message';
+import styles from './MessageBody.module.css';
 
 interface Props {
   message: gmail_v1.Schema$Message;
 }
 
 const MessageBody: React.FC<Props> = ({ message }) => {
+  const darkModeLayer = `<div class="${styles.darkModeLayer}"></div>`;
+  const content = `${darkModeLayer}${getBodyHTML(message)}`;
   return (
-    <div
-      // eslint-disable-next-line react/no-danger
-      dangerouslySetInnerHTML={{ __html: getBodyHTML(message) }}
-    />
+    <div className="relative">
+      <div
+        className={styles.message}
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
+    </div>
   );
 };
 
