1a7c1d55aaf9451d66fa86e0c8b96569ceb3d9e7
GriffinSauce <jorisjgriffioen@gmail.com>
2021-02-28 00:57:36 +0100

    Prevent over-refetching

--- src/hooks/useMessages.ts
+++ src/hooks/useMessages.ts
@@ -14,6 +14,7 @@ function useMessages(options: UseQueryOptions<ResponseData> = {}) {
   return useQuery<ResponseData, ResponseError>('messages', fetchMessages, {
     ...options,
     enabled: isAuthenticated && options.enabled,
+    staleTime: 1000 * 60, // ms
   });
 }
 
