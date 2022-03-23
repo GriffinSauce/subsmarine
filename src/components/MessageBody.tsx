84e51734076a09c484b63200561f79e86aaa9d38
Joris Griffioen <jorisjgriffioen@gmail.com>
2022-03-23 21:38:02 +0100

    Use Interweave to render and make all links target=_blank

--- src/components/MessageBody.tsx
+++ src/components/MessageBody.tsx
@@ -1,11 +1,20 @@
 import { Email } from 'mailslurp-client';
 import { useTheme } from 'next-themes';
-import { getBodyHTML } from 'utils/message';
+import { Interweave, FilterInterface } from 'interweave';
 import styles from './MessageBody.module.css';
 
 interface Props {
   message: Email;
 }
+
+const linkFilter: FilterInterface = {
+  node(name, node) {
+    if (name === 'a') {
+      node.setAttribute('target', '_blank');
+    }
+    return node;
+  },
+};
 
 const MessageBody: React.FC<Props> = ({ message }) => {
   const { resolvedTheme } = useTheme();
@@ -14,15 +23,12 @@ const MessageBody: React.FC<Props> = ({ message }) => {
     resolvedTheme === 'dark'
       ? `<div class="${styles.darkModeLayer}"></div>`
       : '';
-  const content = `${darkModeLayer}${getBodyHTML(message)}`;
+  const contentLayer = `<div class="${styles.message}">${message.body}</div>`;
+  const content = `${darkModeLayer}${contentLayer}`;
 
   return (
     <div className="relative">
-      <div
-        className={styles.message}
-        // eslint-disable-next-line react/no-danger
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
+      <Interweave content={content} filters={[linkFilter]} />
     </div>
   );
 };
