b9ba2178adbf6440556c6b9c66cc60c67cfcdd06
Joris Griffioen <jorisjgriffioen@gmail.com>
2022-04-10 20:42:58 +0200

    Remove pull to refresh for now

--- src/pages/subs/[[...id]].tsx
+++ src/pages/subs/[[...id]].tsx
@@ -3,32 +3,20 @@ import Layout from 'components/Layout';
 import MessageList from 'components/MessageList';
 import Message from 'components/Message';
 import MessageListLoader from 'components/MessageListLoader';
-import PullToRefresh from 'components/PullToRefresh';
 import { Media } from 'components/Media';
 import useSelectedMessageId from 'hooks/useSelectedMessageId';
 import useRedirectUnauthenticated from 'hooks/useRedirectUnauthenticated';
-import useMessages from 'hooks/useMessages';
 
 const Page: NextPage = () => {
   useRedirectUnauthenticated('/');
 
   const messageId = useSelectedMessageId();
-  const { refetch } = useMessages();
 
   return (
     <Layout className="lg:max-h-screen" withFooter={false}>
       <div className="container flex flex-col flex-grow min-h-0 px-3 py-4 mx-auto space-y-3">
         <Media lessThan="lg" className="relative min-h-0">
-          {messageId ? (
-            <Message id={messageId} />
-          ) : (
-            <>
-              <PullToRefresh onPull={refetch}>
-                <MessageList />
-              </PullToRefresh>
-              <MessageListLoader />
-            </>
-          )}
+          {messageId ? <Message id={messageId} /> : <MessageList />}
         </Media>
         <Media greaterThanOrEqual="lg" className="flex flex-row min-h-0 -mr-2">
           <div className="relative w-1/3 min-h-0 -ml-2 overflow-hidden">
