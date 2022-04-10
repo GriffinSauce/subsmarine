22e85f9d9c7c447a8fc9bbbbcfc705bbdbc45a6a
Joris Griffioen <jorisjgriffioen@gmail.com>
2022-04-10 22:11:26 +0200

    We're using Interweave now to render

--- src/utils/message.ts
+++
@@ -1,8 +0,0 @@
-import DOMPurify from 'dompurify';
-import { Email } from 'mailslurp-client';
-
-// eslint-disable-next-line import/prefer-default-export
-export const getBodyHTML = (message: Email): string => {
-  const dirtyHTML = message.body;
-  return DOMPurify.sanitize(dirtyHTML, { USE_PROFILES: { html: true } });
-};
